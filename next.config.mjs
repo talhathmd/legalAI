b8a0976407ddc79a3a87bd484911f266049e6c16
Atiqur Rahman <atiqur.main@gmail.com>
2024-10-05 23:27:51 -0500

    implement pdf rendering after upload

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
     },
     serverComponentsExternalPackages: ["mongoose"],
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
 };
 
 export default nextConfig;
